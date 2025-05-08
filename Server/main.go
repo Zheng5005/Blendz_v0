5afdfb1e606469881b9dc7b70be0364cd4f273b9
Zheng5005 <u.gomezherrera@gmail.com>
2025-05-08 12:19:41 -0600

    Gin usage

--- Server/main.go
+++ Server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+  "github.com/gin-gonic/gin"
+  "net/http"
+)
 
-func main(){
-	fmt.Println("Hello world")
+func main() {
+  router := gin.Default()
+
+  router.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
+
+  router.Run() // listen and serve on 0.0.0.0:8080
 }
