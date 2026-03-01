5ace9a3d9c84ca64e928ddb2ef6dde64be106401
Zheng5005 <u.gomezherrera@gmail.com>
2026-03-01 13:45:25 -0600

    Fixing CORS Issues

--- Server/middleware/cors.go
+++ Server/middleware/cors.go
@@ -6,7 +6,7 @@ import "net/http"
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Adjust the origin as needed
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://blendz-v0.vercel.app")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
